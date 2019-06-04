c24ecedd942956aab68082f410a3aebd010539f4
Suzuna Kobayashi <s.kobayashi1811@morijyobi.ac.jp>
2019-06-04 11:13:52 +0900

    [modify]Conflict.java

--- Conflict.java
+++ Conflict.java
@@ -1,1 +1,12 @@
-HelloWorld
+List<Integer> numlist= new LinkedList<Integer>();
+for(int i=0;i<100;i++)numlist.add(i+1);
+//こっから乱数生成
+Random rand=new Random();
+int kekka[]=new int[10];
+for(int i=0;i<10;i++){
+int index=rand.nextInt(numlist.size());
+kekka[i]=numlist.remove(index);
+}
+for(int i:kekka){
+System.out.println(i);
+}
